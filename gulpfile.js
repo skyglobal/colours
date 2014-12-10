60e21f9f2f6fb2a354efd7bd17c16295102eb062
Peter Mouland <peter.mouland@gmail.com>
2014-12-10 21:44:47 +0000

    update gulp-component-helper + add github link

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
-var pkg = require('./package.json');
-pkg.refresh = function (){ return require('./package.json');}
-var skyComponentHelper = require('gulp-sky-component-helper')(gulp, pkg);
-var paths = skyComponentHelper.paths;
+var componentHelper = require('gulp-component-helper')(gulp);
+var paths = componentHelper.paths;
 var browserSync = require('browser-sync');
 var runSequence = require('run-sequence');
-var rename = require("gulp-rename");
-
-gulp.task('pre-build', function(cb){
-
-    return gulp.src('bower_components/bskyb-typography/dist/css/main.css')
-        .pipe(rename('typography.css'))
-        .pipe(gulp.dest('_site/css/'))
-
-});
